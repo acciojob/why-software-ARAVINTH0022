3ebd58d78e90f64fe2c49f23227b471b79d6cd1e
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-07-15 15:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const body = document.querySelector('body');
+
+const h1 = document.createElement('h1');
+h1.innerText = "Why I want to become a developer?";
+body.appendChild(h1);
+
+const p = document.createElement('p');
+p.innerText = "I want to become a developer because I enjoy solving problems and building useful applications that can help people in their daily lives.";
+body.appendChild(p);   
